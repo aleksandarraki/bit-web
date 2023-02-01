c90c9404c56688b6267d05b8c89da0eef7ce81f9
aleksandar <aleksandarrakic@yahoo.com>
2023-02-01 22:08:02 +0100

    new

---
+++ DOM/Events/toggle-bckground4/script.js
@@ -0,0 +1,26 @@
+// Toggle colors random!!
+
+
+// svaka hexacolor može da neke od vrednosti u nizu "hexcolor"
+
+var hexColor = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+var btn = document.getElementById("btn");
+
+function toggleBackground() {
+    // svaka hexacolor počinje sa "#", to je fiksirana vrednost
+    var newvColor = "#";
+
+    // i može imati 6 vrednosti iz niza "hexa"
+    for (var i = 0; i < 6; i++) {
+        newvColor += hexColor[Math.floor(Math.random() * hexColor.length)];
+    }
+
+    document.getElementById("demo").textContent = "color: " + newvColor;
+    document.querySelector("div").style.backgroundColor = newvColor;
+
+}
+
+
+btn.addEventListener("click", toggleBackground);
+
+
